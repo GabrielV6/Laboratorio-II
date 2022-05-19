15ebeac0dff704d3b6ecf01f033b7a23c9352b53
tfdace <58187350+tfdace@users.noreply.github.com>
2022-05-19 17:51:34 -0300

    Se agrega .h de Clase TarifaV

---
+++ InterlocutorComercial/TarifaV.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "TarifaRN.h"
+
+class TarifaV
+{   
+    private:
+    Tarifa tarifa;
+    string nombreArchivo;
+    TarifaRN tarifaRN;
+    public:
+    TarifaV(string nombreArchivo);
+    ~TarifaV();
+    void NuevaTarifa();
+    void ListarTarifa();
+    void ModificarTarifa();
+    void MenuModificarTarifa();
+    void MenuTarifa();
+    void setNombreArchivo(string nombreArchivo);
+    string getNombreArchivo();
+    void setTarifa(Tarifa& tarifa);
+};
