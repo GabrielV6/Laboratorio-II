4926c495dc5d0c2c59fb9db0e69f07a6e1da4e78
tfdace <58187350+tfdace@users.noreply.github.com>
2022-05-31 21:08:57 -0300

    Modificaciones en TarifaAD.h

--- InterlocutorComercial/TarifaAD.h
+++ InterlocutorComercial/TarifaAD.h
@@ -8,32 +8,18 @@ public:
 	TarifaAD(string nombreArchivo = "");
 	~TarifaAD();
 	void setNombreArchivo(string nombreArchivo);
+
 	string getNombreArchivo();
-	/// <summary>
-	/// M�todo que devuelve la cantidad de registro de Tarifas que hay en el archivo de datos.
-	/// </summary>
-	/// <returns>entero largo</returns>
-	long TotalTarifaEnArchivo();
-	/// <summary>
-	/// M�todo que guarda la tarifa en el archivo de datos y devuelve verdadero cuando resulta correto la grabaci�n.
-	/// </summary>
-	/// <returns>Booleano</returns>
+
+	int TotalTarifaEnArchivo();
 	bool GuardarEnArchivoTarifa(Tarifa& tarifa);
-	/// <summary>
-	/// Este m�todo permite modificar en el archivo la tarifa enviado por parametro
-	/// </summary>
-	/// <param name="tarifa"></param>
-	/// <returns></returns>
 	bool ActualizarEnArchivoTarifa(Tarifa& tarifa);
-	/// <summary>
-	/// M�todo que devuleve un objeto tipo Tarifa que este dentro del archivo de datos
-	/// </summary>
-	/// <param name="posicion"></param>
-	/// <returns>Tarifa</returns>
-	Tarifa getInterlocutorTarifa(int codigoDeTarifa);
-	/// <summary>
-	/// Funci�n que devuleve todas tarifas del archivo en un vector ordenado por codigo de tarifa
-	/// </summary>
+	Tarifa getTarifaArchivo(int codigoDeTarifa);
+	vector<Tarifa> getTarifaArchivo();
+private:
+	string nombreArchivo;
+	Tarifa tarifa;
+};
 	vector<Tarifa> getTarifasArchivo();
 private:
 	string nombreArchivo;
