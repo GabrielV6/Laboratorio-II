a199b55323909cef8542f2768065a0bf035a4060
tfdace <58187350+tfdace@users.noreply.github.com>
2022-05-31 21:12:34 -0300

    Modificaciones en TarifaRN.h

--- InterlocutorComercial/TarifaRN.h
+++ InterlocutorComercial/TarifaRN.h
@@ -6,12 +6,17 @@ private:
 	TarifaAD tarifaAD;
 	string nombreArchivo;
 public:
-	TarifaRN(string nombreArchivo = "");
+	TarifaRN(string nombreArchivo);
+	TarifaRN();
 	~TarifaRN();
 	Tarifa BuscarTarifa(int codigoDeTarifa);
-	bool ControlModificaciones(Tarifa& tarifa);
-	bool AltaCuentaTarifa(Tarifa& tarifa);
-	long CantidadTarifaEnSistema();
+
+	bool AltaTarifa(Tarifa& tarifa);
+	int CantidadTarifaEnSistema();
+    bool ModificarTarifa(Tarifa& tarifa);
+    bool ExisteTarifa(int id_tarifa);
+
+
 	string getNombreArchivo();
 	vector<Tarifa> VectorTarifa();
 	void setNombreArchivo(string nombreArchivo);
