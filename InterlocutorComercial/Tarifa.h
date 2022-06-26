9b415681aa551184de1119662103ca510e22bd68
Gabriel De Jesús Vargas Pabon <gabriel.vargas@alumnos.frgp.utn.edu.ar>
2022-06-26 13:41:58 -0300

    Cambio de archivo Tarifas y cambio de tipo de dato para "Tipo de tarifa" por entero.
    Gabriel Vargas.

--- InterlocutorComercial/Tarifa.h
+++ InterlocutorComercial/Tarifa.h
@@ -11,18 +11,18 @@ private:
 	float _cargoFijo;
 	float _cargoVariable;
 	float _impuestos;
-	bool _tipoDeTarifa; // Domestico (true) o Industrial (false)
+	int _tipoDeTarifa; // Domestico (0) o Industrial (1)
 	int _numPosicionArch;
 	bool _estado;
 public:
 
-	Tarifa(int id = 0, float cargoFijo = 0, float cargoVar = 0, float impuesto = 0, bool tipoTarifa = true, int numPosicion = 0, bool estado = false);
+	Tarifa(int id = 0, float cargoFijo = 0, float cargoVar = 0, float impuesto = 0, int tipoTarifa = 0, int numPosicion = 0, bool estado = true);
 	~Tarifa();
 	void setIdTarifa(int idTarifa);
 	void setCargoFijo(float cargoFijo);
 	void setCargoVariable(float cargoVariable);
 	void setImpuestos(float impuestos);
-	void setTipoDeTarifa(bool tipoDeTarifa);
+	void setTipoDeTarifa(int tipoDeTarifa);
 	void setNumPosicionArchivo(int numPosArch);
 	void setEstado(bool estado);
 
@@ -30,7 +30,7 @@ public:
 	float getCargoFijo();
 	float getCargoVariable();
 	float getImpuestos();
-	bool getTipoDeTarifa();
+	int getTipoDeTarifa();
 	int getNumPosicionArhivo();
 	bool getEstado();
 
